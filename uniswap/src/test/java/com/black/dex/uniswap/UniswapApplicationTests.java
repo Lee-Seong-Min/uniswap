1e1ebc6f88b8ff0011d23c0610be5779268eceed
lhj01 <lhj01@175.116.14.252>
2022-06-18 08:17:48 +0900

    저장

---
+++ uniswap/src/test/java/com/black/dex/uniswap/UniswapApplicationTests.java
@@ -0,0 +1,13 @@
+package com.black.dex.uniswap;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UniswapApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
